b2764cbbf23f13cbd12c76da9be260a2d58d3807
2760walter@gmail.com <2760walter@gmail.com>
2022-01-13 13:42:52 +0300

    Practicing break and continue

--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ namespace CSharpTut
     {
         static void Main(string[] args)
         {
+            //int a;
+            //console.writeline("enter the base number: ");
+            //a = int.parse(console.readline());
+            //console.writeline("enter number 1");
+            //a += int.parse(console.readline());
+            //console.writeline("enter number 2");
+            //a -= int.parse(console.readline());
+            //console.writeline("enter number 3");
+            //a *= int.parse(console.readline());
+            //console.writeline("enter number 4");
+            //a /= int.parse(console.readline());
+
+            //console.writeline("results: {0}", a);
+
+            //*****************************to practice break and continue in for loop***********//
+
+            //for (int i=0; i<100; i++)
+            //{
+
+            //    if (i == 5)
+            //        continue;
+            //    Console.WriteLine(i);
+
+
+            //}
+
+            Console.ReadLine();
+
         }
     }
 }
